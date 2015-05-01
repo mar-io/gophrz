764c92f08628422529c72718fd636cff1f38bf82
Mario Harvey <mario.j.harvey@accenture.com>
2015-05-01 11:04:25 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,59 @@
+#goShell
+
+##What is goShell?
+
+goShell is simple a bash script that will install go on your linux system.
+
+The script supports bash and zsh.
+
+You can install different go versions and manage them in your shell.
+You can switch between different go versions.
+You can completely uninstall/purge go from your system.
+
+By default, the script will find the latest version of 64-bit Go and install to your system.
+
+##How to Use goShell
+
+Download goShell bash script:
+
+`wget (goShell)[]`
+
+To see script usage and help information:
+
+`./goShell.sh -h
+
+To install LATEST go version(64-bit default):
+
+`./goShell.sh`
+
+To install a different version(64-bit default):
+`./goShell.sh -v 1.3.1`
+
+To install 32 bit(will install LATEST):
+`./goShell.sh -b 32`
+
+To install different version and 32 bit:
+`./goShell.sh -v 1.3.1 -b 32`
+
+##Switching Go Versions
+
+To switch versions simply specify the version you want to switch to:
+`./goShell.sh -v 1.4.1`
+
+goShell will install the specified version if its not on your system. However, if you have already installed that version in the past it simply will switch to it by changing GOROOT on your system.
+
+**You may have to log out and back into the shell for changes to take effect.**
+
+## Basic Usage
+
+     usage: $0 [ -v version -b 32/64 -d -p -h ]
+        -n --> Particular Go version < ex. 0.4.2
+        -b --> Bit Version of Go < 32 or 64(default)
+        -d --> Latest Stable Go version
+        -p --> Deletes Go in its entirety from your system
+        -h --> print this help screen
+
+## Issues
+
+Any weird issues or concerns please let me know in the project issues.
+
---
+++ goShell.sh
@@ -0,0 +1,162 @@
+#! /usr/bin/env bash
+set -e
+[ $# -eq 0 ] && set -- -d date > /dev/null
+GETVRS=$(curl -s https://golang.org/doc/devel/release.html | grep 'Build version' | awk '{ print $3 }' | grep -o "[0-9.][0-9.][0-9.]..")
+
+while getopts 'v:b::dph' OPT; do
+  case $OPT in
+    v)  VERSION=$OPTARG;;
+    b)  BIT=$OPTARG;;
+    d)  VERSION=$GETVRS;;
+    p)  PURGE=true;;
+    h)  hlp="yes";;
+    *)  unknown="yes";;
+  esac
+done
+
+# usage
+HELP="
+    usage: $0 [ -v version -b 32/64 -d -p -h ]
+        -n --> Particular Go version < ex. 0.4.2
+        -b --> Bit Version of Go < 32 or 64(default)
+        -d --> Latest Stable Go version
+        -p --> Deletes Go in its entirety from your system
+        -h --> print this help screen
+"
+
+if [ "$hlp" = "yes" ]; then
+  echo "$HELP"
+  exit 0
+fi
+
+if [ "$PURGE" = true ] ; then
+  read -r -p "Are you sure you want to delete go? This will delete EVERYTHING![y/N] " yn
+  case $yn in
+        [Yy]* ) rm -rf "$HOME/.go/"
+                rm -rf "$HOME/go/"
+                if [ "$SHELL" = "/bin/bash" ] ; then
+                  sed -i '/# GoLang/d' "$HOME/.bashrc"
+                  sed -i '/export GOROOT/d' "$HOME/.bashrc"
+                  sed -i '/:$GOROOT/d' "$HOME/.bashrc"
+                  sed -i '/export GOPATH/d' "$HOME/.bashrc"
+                  sed -i '/:$GOPATH/d' "$HOME/.bashrc"
+                  echo "Go purged!"
+                elif [ "$SHELL" = "/bin/zsh" ] ; then
+                  sed -i '/# GoLang/d' "$HOME/.zshrc"
+                  sed -i '/export GOROOT/d' "$HOME/.zshrc"
+                  sed -i '/:$GOROOT/d' "$HOME/.bashrc"
+                  sed -i '/export GOPATH/d' "$HOME/.zshrc"
+                  sed -i '/:$GOPATH/d' "$HOME/.zshrc"
+                  echo "Go purged!"
+                fi
+                exit 0;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+  exit
+fi
+
+if [ "$BIT" = '64' ] ; then
+    if [ -z "$VERSION" ] ; then
+    VERSION=$GETVRS
+    fi
+    BINARY="go$VERSION.linux-amd$BIT.tar.gz"
+elif [ "$BIT" = '32' ] ; then
+    if [ -z "$VERSION" ] ; then
+    VERSION=$GETVRS
+    fi
+    BINARY="go$VERSION.linux-386.tar.gz"
+elif [ -z "$BIT" ] ; then
+    if [ -z "$VERSION" ] ; then
+    VERSION=$GETVRS
+    fi
+    BIT='64'
+    BINARY="go$VERSION.linux-amd$BIT.tar.gz"
+else
+  echo 'Not a valid bit version. Please select 32,64 or nothing at all.Exiting.'
+  exit 1
+fi
+
+LINK="https://storage.googleapis.com/golang/$BINARY"
+TMP="/tmp"
+GOGOGO="$HOME/.go/go-$VERSION-$BIT"
+
+function goswitch {
+
+if [ "$SHELL" = "/bin/bash" ] ; then
+  if cat $HOME/.bashrc | grep "GOROOT" > /dev/null ; then
+    OLD=$(cat $HOME/.bashrc | grep "GOROOT" | grep -o "[0-9.][0-9.][0-9.]..")
+    sed -i "s@GOROOT=$HOME/.go/go-$OLD-$BIT@GOROOT=$GOGOGO@g" $HOME/.bashrc
+    echo "Type 'source ~/.bashrc' to start using go$VERSION"
+  fi
+elif [ "$SHELL" = "/bin/zsh" ]; then
+  if cat $HOME/.zshrc | grep "GOROOT" > /dev/null ; then
+    OLD=$(cat $HOME/.zshrc | grep "GOROOT" | grep -o "[0-9.][0-9.][0-9.]..")
+    sed -i "s@GOROOT=$HOME/.go/go-$OLD-$BIT@GOROOT=$GOGOGO@g" $HOME/.zshrc
+    echo "Type 'source ~/.zshrc' to start using go$VERSION"
+  fi
+elif [ "$SHELL" != "/bin/bash" ] || [ "$SHELL" != "/bin/zsh" ] ; then
+    echo "Not a valid shell. Use bash or zsh"
+    exit 1
+fi
+}
+
+function goshell {
+
+if [ "$SHELL" = "/bin/bash" ] ; then
+  if ! cat $HOME/.bashrc | grep "GOROOT" > /dev/null ; then
+    {
+    echo '# GoLang'
+    echo "export GOROOT=$GOGOGO"
+    echo 'export PATH=$PATH:$GOROOT/bin'
+    echo 'export GOPATH=$HOME/go'
+    echo 'export PATH=$PATH:$GOPATH/bin'
+    } >> "$HOME/.bashrc"
+    echo "Type 'source ~/.bashrc' to start using go$VERSION"
+    exit 0
+  fi
+elif [ "$SHELL" = "/bin/zsh" ]; then
+  if ! cat $HOME/.zshrc | grep "GOROOT" > /dev/null ; then
+ {
+    echo '# GoLang'
+    echo "export GOROOT=$GOGOGO"
+    echo 'export PATH=$PATH:$GOROOT/bin'
+    echo 'export GOPATH=$HOME/go'
+    echo 'export PATH=$PATH:$GOPATH/bin'
+    } >> "$HOME/.zshrc"
+    echo "Type 'source ~/.zshrc' to start using go$VERSION"
+    exit 0
+  fi
+elif [ "$SHELL" != "/bin/bash" ] || [ "$SHELL" != "/bin/zsh" ] ; then
+    echo "Not a valid shell. Use bash or zsh"
+    exit 1
+fi
+}
+
+if go version 2> /dev/null | grep "go$VERSION linux/amd$BIT" > /dev/null ; then
+  echo "Go version $VERSION-$BIT already installed."
+  goshell
+  goswitch
+  exit 0
+elif ls $HOME/.go 2> /dev/null | grep "go-$VERSION-$BIT" > /dev/null ; then
+  echo "Switching Go to version $VERSION-$BIT"
+  goshell
+  goswitch
+  exit 0
+fi
+
+cd $TMP
+echo "Downloading $LINK"
+curl -O $LINK
+if [ $? -ne 0 ]; then
+    echo "Download failed! Are you sure you are using a valid version? Exiting..."
+    exit 1
+fi
+tar -C $HOME -xzf $TMP/$BINARY
+rm -rf $TMP/$BINARY
+mkdir -p $HOME/.go
+mv $HOME/go $GOGOGO
+mkdir -p $HOME/go
+echo "Go version $VERSION installed!"
+goshell
+goswitch

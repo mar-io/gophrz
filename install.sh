53d8e48ca63450511e0ebfc1f8a6940a5d7b9d8a
Mario Harvey <mario.j.harvey@accenture.com>
2015-05-04 21:35:30 -0500

    updating readme and package install

--- install.sh
+++ install.sh
@@ -52,10 +52,10 @@ EOF
   case "$lsb_dist" in
     amzn|fedora|centos)
       $sh_c "yum update && yum -y install curl wget tar"
-      $sh_c "wget -P /usr/bin https://github.com/badmadrad/gophrz/raw/master/binary/gophrz";;
+      $sh_c "wget -O /usr/bin/gophrz https://github.com/badmadrad/gophrz/raw/master/binary/gophrz";;
     ubuntu|debian|linuxmint)
       $sh_c "apt-get update && apt-get -y install curl wget tar"
-      $sh_c "wget -P /usr/bin https://github.com/badmadrad/gophrz/raw/master/binary/gophrz";;
+      $sh_c "wget -O /usr/bin/gophrz https://github.com/badmadrad/gophrz/raw/master/binary/gophrz";;
     osx)
       $sh_c "wget -O /usr/bin/gophrz https://github.com/badmadrad/gophrz/raw/master/binary/gophrz-mac";;
   esac
